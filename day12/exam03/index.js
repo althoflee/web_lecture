dae07092dd8de4fdfaf06d9670e0d89dd7fe17ba
gbox3dgel3 <gbox3dgel3@gmail.com>
2024-11-12 10:07:23 +0900

    ...

--- day12/exam03/index.js
+++ day12/exam03/index.js
@@ -21,14 +21,42 @@ function foo(name,score) {
     }
 }
 
+function bar(name,score) {
+    this.name = name
+    this.score = score
+}
+
+bar.prototype.getGrade = function() {
+    if(this.score > 90) {
+        return "A"
+    }
+    else if(this.score > 80) {
+        return "B"
+    }
+    else if(this.score > 70) {
+        return "C"
+    }
+    else if(this.score > 60) {
+        return "D"
+    }
+    else {
+        return "F"
+    }
+}
+
 export default async function main() {
     console.log("hello es6");
 
     let _student1 = new foo("Alice",85)
+    let _student1_2 = new foo("Adam",95)
 
     console.log(_student1)
 
     console.log(_student1.getGrade())
+
+    let _student2 = new bar("jenny",75)
+    console.log(_student2)
+    console.log(_student2.getGrade())
 
 
 }

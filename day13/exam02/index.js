47cf428b6adaf0a4a47359e2c9b21febd843b2ad
gbox3dgel3 <gbox3dgel3@gmail.com>
2024-11-12 10:18:10 +0900

    .....

--- day13/exam02/index.js
+++ day13/exam02/index.js
@@ -1,3 +1,20 @@
+var counter = 0;
+
 export default async function main() {
-    console.log("hello es6");
+
+    let _counter = document.querySelector("#counter h2")
+
+    console.log(_counter)
+
+    let _btn_inc = document.querySelector("#btn-inc")
+
+    _btn_inc.addEventListener('click',()=> {
+
+        counter++
+        console.log(counter)
+        _counter.innerText = counter
+    })
+    
+
+
 }
